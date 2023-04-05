9c3033df900a06dc42c5271ae1d561d169c2819e
Lucas Ribeiro De Lacerda <32784630803@info.local>
2023-04-05 09:47:50 -0300

    back end aula 05/04

---
+++ 04-04 - Estruturas de Repetição - While, Do While, For/Program.cs
@@ -0,0 +1,59 @@
+﻿
+//Faça um programa que leia e valide as seguintes informações:
+
+// 1. Nome: diferente de vazio;
+
+// 2. Idade: entre 0 e 100 anos;
+
+// 3. Salário: maior que zero;
+
+// 4. Estado Civil: 's'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd'(divorciado(a));
+
+Console.Write("Informe seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Bem vindo " + nome );
+
+while (nome == "")
+{
+    Console.WriteLine("O nome não pode ser vazio.");
+    nome = Console.ReadLine();
+}
+
+
+Console.Write("Informe sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+while (idade < 0 || idade > 100)
+{
+    Console.WriteLine("A idade deve estar entre 0 e 100 anos.");
+    idade = int.Parse(Console.ReadLine());
+}
+
+
+Console.Write("Informe seu salário: ");
+float salario = float.Parse(Console.ReadLine());
+
+while (salario <= 0)
+{
+    Console.WriteLine("O salário deve ser maior que zero.");
+      salario = float.Parse(Console.ReadLine());
+}
+
+
+Console.Write($"Informe seu estado Civil 's'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd'(divorciado(a)): ");
+char estadoCivil = char.Parse(Console.ReadLine().ToLower()) ;
+
+while (estadoCivil != 's' && estadoCivil != 'c' && estadoCivil != 'v' && estadoCivil != 'd')
+{
+    Console.WriteLine("O estado civil deve ser 's', 'c', 'v' ou 'd'.");
+    estadoCivil = char.Parse(Console.ReadLine().ToLower()) ; 
+}
+
+
+ Console.WriteLine("Informações válidas.");
+
+
+
+
+
--- menu de opções de bebidas/Program.cs
+++ menu de opções de bebidas/Program.cs
@@ -1,16 +1,16 @@
-﻿// //Crie um menu de opções de bebidas e peça para o usuário escolher uma das opções.
+﻿// Crie um menu de opções de bebidas e peça para o usuário escolher uma das opções.
 // A depender da opção, exiba uma mensagem se gostaria de acrescentar gelo.
 // Se sim, exiba uma mensagem que a bebida em questão será com adicional de gelo, caso contrário, a bebida é sem gelo adicional.
 
 
-    // Exibe o menu de bebidas e solicita a escolha do usuário
+// Exibe o menu de bebidas e solicita a escolha do usuário
 
-    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
 
-    Console.WriteLine(@$"
+Console.WriteLine(@$"
              Bem vindo a Destilaria Barão!!
     ");
-    Console.WriteLine(@$"
+Console.WriteLine(@$"
    _____________________________________________________ 
    |                 MENU DE BEBIDAS                   |
    |                 1 - Água                          |
@@ -21,49 +21,53 @@
    _____________________________________________________
 
     ");
-    Console.ResetColor();
+Console.ResetColor();
 
-    Console.ForegroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Blue;
 
-    Console.Write("Escolha uma opção: ");
-    int escolha = int.Parse(Console.ReadLine());
+Console.Write("Escolha uma opção: ");
+int escolha = int.Parse(Console.ReadLine());
 
-    Console.ResetColor();
+Console.ResetColor();
 
-    // Verifica a escolha do usuário e solicita a adição de gelo se necessário
-    switch (escolha) 
-    {
-      case 1:
+// Verifica a escolha do usuário e solicita a adição de gelo se necessário
+switch (escolha)
+{
+    case 1:
         Console.Write("Gostaria de adicionar gelo à água? (S/N): ");
         break;
-      case 2:
+    case 2:
         Console.Write("Gostaria de adicionar gelo ao refrigerante? (S/N): ");
         break;
-      case 3:
+    case 3:
         Console.Write("Gostaria de adicionar gelo ao suco? (S/N): ");
         break;
-      case 4:
+    case 4:
         Console.Write("Gostaria de adicionar gelo à Batida? (S/N): ");
         break;
-      case 5:
+    case 5:
         Console.Write("Gostaria de adicionar gelo ao Destilado? (S/N): ");
         break;
-      default:
+    default:
         Console.WriteLine("Opção inválida!");
         return;
-    }
+}
 
-    // Verifica se o usuário deseja adicionar gelo ou não
-  Console.ForegroundColor = ConsoleColor.Blue;
+// Verifica se o usuário deseja adicionar gelo ou não
+Console.ForegroundColor = ConsoleColor.Blue;
 
-    string resposta = Console.ReadLine().ToUpper();
-    if (resposta == "S") 
-    {
-      Console.WriteLine("Bebida com adicional de gelo!");
-    } else 
-    {
-      Console.WriteLine("Bebida sem gelo adicional.");
-    }
+string resposta = Console.ReadLine().ToUpper();
+if (resposta == "S")
+{
+    Console.WriteLine("Bebida com adicional de gelo!");
+}
+else
+{
+    Console.WriteLine("Bebida sem gelo adicional.");
+}
 
-    Console.ResetColor();
+Console.ResetColor();
+
+
+
 
