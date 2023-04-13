1f5f9c37b910d570a6602b56d576cfc9a2abd8c7
Lucas Ribeiro De Lacerda <32784630803@info.local>
2023-04-13 10:51:45 -0300

    aula 13/04 funções exercicios

--- aula 12-04 funçoes/exercicio 2/Program.cs
+++ aula 12-04 funçoes/exercicio 2/Program.cs
@@ -1,42 +1,30 @@
 ﻿// 1 - Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se
 // ela poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).
 
+//tentar com dateTime
+// Console.WriteLine($"Digite seu ano de nascimento: ");
+// int anoDeNascimento = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Digite seu ano de nascimento: ");
-int anoDeNascimento = int.Parse(Console.ReadLine());
+// var dataAtual =  DateTime.Now;
 
-var dataAtual =  DateTime.Now;
+// int permitidoVotar = dataAtual - anoDeNascimento;
 
-int permitidoVotar = dataAtual - anoDeNascimento;
 
-if (anoDeNascimento >= 18)
+Console.Write("Digite o ano atual: ");
+int anoAtual = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o ano de nascimento: ");
+int anoNascimento = int.Parse(Console.ReadLine());
+
+int idade = anoAtual - anoNascimento;
+
+if (idade >= 16)
 {
-    Console.WriteLine($"Você pode votar!!");
-
+    Console.WriteLine("Você poderá votar este ano!");
 }
 else
 {
-    Console.WriteLine($"Você não pode votar! Sorte sua");
-
+    Console.WriteLine("Você não poderá votar este ano!");
 }
-
-
-
-// Console.Write("Digite o ano atual: ");
-// int anoAtual = int.Parse(Console.ReadLine());
-
-// Console.Write("Digite o ano de nascimento: ");
-// int anoNascimento = int.Parse(Console.ReadLine());
-
-// int idade = anoAtual - anoNascimento;
-
-// if (idade >= 16)
-// {
-//     Console.WriteLine("Você poderá votar este ano!");
-// }
-// else
-// {
-//     Console.WriteLine("Você não poderá votar este ano!");
-// }
 
 
