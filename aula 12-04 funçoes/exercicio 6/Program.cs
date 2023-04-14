66a80200d92ea382e012d8d974f488dc9bfc5d15
Lucas Ribeiro De Lacerda <32784630803@info.local>
2023-04-14 08:27:31 -0300

    aula 14/04 funções exercicios

--- aula 12-04 funçoes/exercicio 6/Program.cs
+++ aula 12-04 funçoes/exercicio 6/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Escreva um algoritmo que imprima a tabuada (de 1 a 10) para os números de 1 a 10.
 // Exemplo: tabuada do 1, tabuada do 2, etc... Dica: utilize um laço dentro do outro.
 
+
+
+for(int i=1; i<=10; i++) // loop externo para os números de 1 a 10
+{
+    Console.WriteLine("Tabuada do " + i + ":"); // imprime a mensagem da tabuada
+    for(int j=1; j<=10; j++) // loop interno para a tabuada de 1 a 10
+    {
+        Console.WriteLine(i + " x " + j + " = " + i*j); // imprime a linha da tabuada
+    }
+    Console.WriteLine(); // imprime uma linha em branco para separar as tabuadas
+}
---
+++ aula 12-04 funçoes/exercicio 7/Program.cs
@@ -0,0 +1,35 @@
+﻿// 6 - Escreva um algoritmo que permita a leitura dos nomes de 10 pessoas e armazene os nomes
+// lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de
+// pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver
+// entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário.
+
+
+string[] nomes = new string[10]; // vetor para armazenar os nomes
+for (int i = 0; i < 10; i++) // loop para ler os 10 nomes
+{
+    Console.Write("Digite o nome da pessoa " + (i + 1) + ": ");
+    nomes[i] = Console.ReadLine();
+}
+
+Console.Write("Digite o nome que deseja buscar: ");
+string nomeBusca = Console.ReadLine();
+
+bool nomeEncontrado = false; // variável para indicar se o nome foi encontrado
+for (int i = 0; i < 10; i++) // loop para buscar o nome no vetor
+{
+    if (nomes[i] == nomeBusca)
+    {
+        nomeEncontrado = true;
+        break;
+    }
+}
+
+if (nomeEncontrado)
+{
+    Console.WriteLine($"Nome Encontrado"); // imprime a mensagem de sucesso se o nome foi encontrado
+}
+else
+{
+    Console.WriteLine($"NÃO Encontrado"); // imprime a mensagem de erro se o nome não foi encontrado
+}
+
