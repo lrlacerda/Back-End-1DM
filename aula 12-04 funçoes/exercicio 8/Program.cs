92a6fc077e8a28af994b945bf7cfa4cbb2e61888
Lucas Ribeiro De Lacerda <32784630803@info.local>
2023-04-14 08:42:30 -0300

    aula 14/04 funções exercicios

--- aula 12-04 funçoes/exercicio 8/Program.cs
+++ aula 12-04 funçoes/exercicio 8/Program.cs
@@ -1,3 +1,21 @@
 ﻿// 7 - Faça um algoritmo para ler 15 números e armazenar em um vetor. Após a leitura total dos
 // 15 números, o algoritmo deve escrever esses 15 números lidos na ordem inversa da qual foi
 // declarado.
+
+
+
+int[] numeros = new int[15]; // vetor para armazenar os números
+
+for (int i = 0; i < 15; i++) // loop para ler os 15 números
+{
+    Console.Write("Digite o número " + (i + 1) + ": ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Números na ordem inversa:"); 
+for (int i = 14; i >= 0; i--) // loop para imprimir os números na ordem inversa
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine(numeros[i]);
+    Console.ResetColor();
+}
